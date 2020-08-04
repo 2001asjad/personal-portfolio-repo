197967cdec40324a9b48c02dda0fc9ea8e30a108
Asjad <iitasjad2001@gmail.com>
2020-08-04 06:24:57 +0530

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path,include
+from . import views
+app_name = 'blog'
+urlpatterns = [
+    path('',views.all_blogs,name='all_blogs'),
+    path('<int:blog_id>/',views.detail,name='detail'),
+]
